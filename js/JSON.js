dffce9d39defe157aac3d3f44d5257fe4f931186
Andrew Phillips <andrewtylerphillips@gmail.com>
2012-02-02 01:47:10 -0600

    Rebuilding app from last month, finishing touches on the way

---
+++ js/JSON.js
@@ -0,0 +1,245 @@
+// Andrew Phillips
+// MiU 0212
+// Default/Dummy Data
+
+var jsonDefaults = {
+		"asset1": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset2": {
+			"packagenick": ["Package Name:", "Payment for Laura"],
+			"datesend": ["Ship Date:", "2011-12-20"],
+			"category": ["Package Type:", "Envelope"],
+			"rushorder": ["Expedite Shipment:", "No"],
+			"dontrush": ["Don't Expedite Shipment:", "Yes"],
+			"numberpackages": ["Amount to be Shipped:", "2"],
+			"notes": ["Shipping Notes", "Here are the two payments you requested."],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "No"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset3": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset4": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset5": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset6": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset7": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset8": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset9": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset10": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset11": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset12": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset13": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset14": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset15": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset16": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset17": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset18": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset19": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		},
+		
+		"asset20	": {
+			"packagenick": ["Package Name:", "Tim's CDs"],
+			"datesend": ["Ship Date:", "2011-12-15"],
+			"category": ["Package Type:", "Small Package"],
+			"rushorder": ["Expedite Shipment:", "Yes"],
+			"dontrush": ["Don't Expedite Shipment:", "No"],
+			"numberpackages": ["Amount to be Shipped:", "1"],
+			"notes": ["Shipping Notes", "Here are the CDs you left at my house!"],
+			"shipNotify": ["Send an \'Item Shipped\' notification?", "Yes"],
+			"progressNotify": ["Update progress of shipped package?", "Yes"]
+		}
+};
